d0795aa1f3404d2e0158bc0c5becd5bb3b44e9f7
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-03 15:02:22 -0800

    Set Up React & TailwindCSS with Vite

---
+++ react-ai/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2 className="bg-purple-300 text-white">Hello!</h2>
+    </div>
+  )
+}
+
+export default App
