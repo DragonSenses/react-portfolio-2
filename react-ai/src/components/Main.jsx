9590f824d25bbb9ac4e3b27e13a74c33b5a18227
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-04 01:30:23 -0800

    Update Main.jsx

--- react-ai/src/components/Main.jsx
+++ react-ai/src/components/Main.jsx
@@ -5,7 +5,7 @@ import Skills from './Skills'
 
 export default function Main() {
   return (
-    <div className='p-4 flex flex-col md:grid md:grid-cols-4'>
+    <div className='p-4 flex flex-col flex-1 md:grid md:grid-cols-4 gap-4'>
       <div className="md:col-span-3 flex flex-col gap-4">
         <AboutMe />
         <Experience />
