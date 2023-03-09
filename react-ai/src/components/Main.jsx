eaf18176045f880d85ad1200a875fa347d0c8a10
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-08 17:37:56 -0800

    Update Main.jsx

--- react-ai/src/components/Main.jsx
+++ react-ai/src/components/Main.jsx
@@ -3,12 +3,14 @@ import AboutMe from './AboutMe'
 import Experience from './Experience'
 import Skills from './Skills'
 import { useState } from 'react'
+import Modal from './Modal'
 
 export default function Main() {
-  const [showModal, setShowModal] = useState(false);
-  
+  const [showModal, setShowModal] = useState(true);
+
   return (
     <div className='p-4 flex flex-col flex-1 md:grid md:grid-cols-4 gap-4'>
+      { showModal && <Modal /> }
       <div className="md:col-span-3 flex flex-col gap-4">
         <AboutMe />
         <Experience />
