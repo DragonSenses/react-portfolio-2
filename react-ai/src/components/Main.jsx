d22c9312343c361267e3ce2b34077fedfabbecd9
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-12 21:24:26 -0700

    Update Main.jsx

--- react-ai/src/components/Main.jsx
+++ react-ai/src/components/Main.jsx
@@ -4,6 +4,7 @@ import Experience from './Experience'
 import Skills from './Skills'
 import { useState } from 'react'
 import Modal from './Modal'
+import Background from './Background'
 
 export default function Main() {
   const [showModal, setShowModal] = useState(false);
@@ -17,12 +18,15 @@ export default function Main() {
   }
 
   return (
-    <div className='p-4 flex flex-col flex-1 md:grid md:grid-cols-4 gap-4'>
+    <div className='p-4 flex flex-col flex-1 md:grid md:grid-cols-4 gap-4
+    relative overflow-hidden'>
       { showModal && <Modal onClose={onCloseHandler} /> }
-      <div className="md:col-span-3 flex flex-col gap-4">
+      <div className="md:col-span-3 z-10 flex flex-col gap-4">
         <AboutMe openModal={openModal} />
         <Experience />
       </div>
+      
+      <Background />
       <Skills />
     </div>
   )
